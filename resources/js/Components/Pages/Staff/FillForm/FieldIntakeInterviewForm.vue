dc496255467123fc25f686c6c9e5b96c39e2b719
TNarido <tristannarido@gmail.com>
2024-03-24 10:31:00 +0800

    Add input field for forms

--- resources/js/Components/Pages/Staff/FillForm/FieldIntakeInterviewForm.vue
+++ resources/js/Components/Pages/Staff/FillForm/FieldIntakeInterviewForm.vue
@@ -1,61 +1,70 @@
 <template>
     <div>
         <div class="container">
-            <h2>Intake Interview Form</h2>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="campus">Campus:</label>
-                        <input type="text" class="form-control" id="campus" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="studentName">Name of Student:</label>
-                        <input type="text" class="form-control" id="studentName" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="nickname">Nickname:</label>
-                        <input type="text" class="form-control" id="nickname" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="elementarySchool">Elementary School Graduated From:</label>
-                        <input type="text" class="form-control" id="elementarySchool" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="schoolAddress">School Address:</label>
-                        <input type="text" class="form-control" id="schoolAddress" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age:</label>
-                        <input type="text" class="form-control" id="age" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="sex">Sex:</label>
-                        <select class="form-control" id="sex" required>
-                            <option value="">Select Sex</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="other">Other</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="dateOfInterview">Date of Interview:</label>
-                        <input type="date" class="form-control" id="dateOfInterview" required>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="interviewedBy">Interviewed By:</label>
-                    <input type="text" class="form-control" id="interviewedBy" required>
-                </div>
-                <div class="form-group">
-                    <label for="comments">Purpose:</label>
-                    <textarea class="form-control" id="comments" rows="3" required></textarea>
-                </div>
+            <div class="text-header">
+                Intake Interview Form
             </div>
-            <div class="row">
-                <div class="col-md-12 text-right">
-                    <button type="submit" class="btn btn-primary btn-submit">Submit</button>
+            <div class="left-right">
+                <div class="left">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="campus">Campus:</label>
+                                <input type="text" class="form-control" id="campus" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="studentName">Name of Student:</label>
+                                <input type="text" class="form-control" id="studentName" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="nickname">Nickname:</label>
+                                <input type="text" class="form-control" id="nickname" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="elementarySchool">Elementary School Graduated From:</label>
+                                <input type="text" class="form-control" id="elementarySchool" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="schoolAddress">School Address:</label>
+                                <input type="text" class="form-control" id="schoolAddress" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="age">Age:</label>
+                                <input type="text" class="form-control" id="age" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="sex">Sex:</label>
+                                <select class="form-control" id="sex" required>
+                                    <option value="">Select Sex</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="dateOfInterview">Date of Interview:</label>
+                                <input type="date" class="form-control" id="dateOfInterview" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="interviewedBy">Interviewed By:</label>
+                            <input type="text" class="form-control" id="interviewedBy" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="comments">Purpose:</label>
+                            <textarea class="form-control" id="comments" rows="3" required></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="right d-flex flex-column align-items-end bts">
+                    <div class="col-md-6 mb-2">
+                        <button type="submit" class="btn btn-primary btn-submit">Submit</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-outline-primary btn-back">Back</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -66,12 +75,31 @@
 </script>
 
 <style scoped>
-.container h2 {
+.container {
+    width: 100%;
+}
+
+.container .text-header {
     margin-bottom: 40px;
     color: #000000b3;
-    font: 700 20px Montserrat, sans-serif;
-    width: 100%;
+    font: 700 25px Montserrat, sans-serif;
     border-bottom-width: 5px;
+}
+
+.left-right {
+    width: 100%;
+    display: flex;
+}
+
+.container .left {
+    width: 70%;
+    background-color: #0b5dd72d;
+    padding: 20px 20px 20px 20px;
+    border-radius: 10px;
+}
+
+.container .right {
+    width: 10%;
 }
 
 .container label {
@@ -84,13 +112,44 @@
     border: 1px solid #9e9e9e;
 }
 
-.btn-submit {
-    width: 250px;
+.btns {
     margin-bottom: 50px;
 }
 
+.btn-submit {
+    width: 250px;
+}
+
+.btn-back {
+    width: 250px;
+    margin-right: 10px;
+}
+
 @media screen and (max-width:360px) {
+    .container .text-header {
+        text-align: center;
+    }
+
+    .left-right {
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .container .left,
+    .container .right {
+        width: 100%;
+    }
+
+    .container .right {
+        margin-bottom: 10px;
+        margin-top: 20px
+    }
+
     .btn-submit {
+        width: 100%;
+    }
+
+    .btn-back {
         width: 100%;
     }
 }
